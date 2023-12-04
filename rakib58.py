2492974381495ae22a8d65116b9dbd8b4cfa37e3
blackhunter39 <152779630+blackhunter39@users.noreply.github.com>
2023-12-05 00:10:10 +0800

    Add files via upload

---
+++ rakib58.py
@@ -0,0 +1,8 @@
+name='nuist';add='nanjing,china';rank=700;stu=15000
+plyers_list=['rakib'' ''tadir'' ''noha']
+age=19
+print (name,add,rank,stu,plyers_list)
+print (11*1,11*2,11*3,11*4,11*5,1+2)
+nums=['1','2','3','45','45','343'];nums[0]
+boys=['rakib']
+print (nums[0])
