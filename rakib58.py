9250216f71c7387a0373931523279361805f3442
blackhunter39 <152779630+blackhunter39@users.noreply.github.com>
2023-12-07 12:08:24 +0800

    Add files via upload

--- rakib58.py
+++ rakib58.py
@@ -1,8 +1,12 @@
-name='nuist';add='nanjing,china';rank=700;stu=15000
-plyers_list=['rakib'' ''tadir'' ''noha']
-age=19
-print (name,add,rank,stu,plyers_list)
-print (11*1,11*2,11*3,11*4,11*5,1+2)
-nums=['1','2','3','45','45','343'];nums[0]
-boys=['rakib']
-print (nums[0])
+from turtle import*
+bgcolor('blue')
+color('black')
+begin_fill()
+pensize(4)
+left(50)
+forward(143)
+circle(50,200)
+right(150)
+circle(50,200)
+forward(154)
+end_fill()
